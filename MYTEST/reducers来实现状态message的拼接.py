190e1901839c559c7ff5fdf199302d084f41ba12
Qiemin <15835010873@163.com>
2026-08-15 21:53:45 +0800

    day2学习

---
+++ MYTEST/reducers来实现状态message的拼接.py
@@ -0,0 +1,26 @@
+from operator import add
+from typing import Annotated
+from typing_extensions import TypedDict
+
+def reduce_list(left: list | None, right: list | None) -> list:
+    """Safely combine two lists, handling cases where either or both inputs might be None.
+
+    Args:
+        left (list | None): The first list to combine, or None.
+        right (list | None): The second list to combine, or None.
+
+    Returns:
+        list: A new list containing all elements from both input lists.
+               If an input is None, it's treated as an empty list.
+    """
+    if not left:
+        left = []
+    if not right:
+        right = []
+    return left + right
+
+class DefaultState(TypedDict):
+    foo: Annotated[list[int], add]
+
+class CustomReducerState(TypedDict):
+    foo: Annotated[list[int], reduce_list]
---
+++ MYTEST/基于基础路由的reAct工具调用.py
@@ -0,0 +1,47 @@
+from langchain_openai import ChatOpenAI
+
+def multiply(a: int, b: int) -> int:
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+llm = ChatOpenAI(model="qwen-plus", base_url="https://dashscope.aliyuncs.com/compatible-mode/v1")
+llm_with_tools = llm.bind_tools([multiply])
+
+
+from IPython.display import Image, display
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph import MessagesState
+from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import tools_condition
+
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode([multiply]))
+builder.add_edge(START, "tool_calling_llm")
+builder.add_conditional_edges(
+    "tool_calling_llm",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", END)
+graph = builder.compile()
+
+# View
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+from langchain_core.messages import HumanMessage
+messages = [HumanMessage(content="你好，100乘1000等于多少")]
+messages = graph.invoke({"messages": messages})
+for m in messages['messages']:
+    m.pretty_print()
